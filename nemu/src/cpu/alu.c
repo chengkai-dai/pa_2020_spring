f3b869a6db0ebd5602b10897807abdc41b2bc6f5
tracer-pa-public <tracer@njuics.org>
2021-06-29 23:33:06 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  23:33:06 up 1 day, 10:30,  2 users,  load average: 0.14, 0.10, 0.03 4584eb208960393e3a30d67cdbc00ec71069204

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -328,13 +328,17 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_imul(src, dest, data_size);
 #else
-	int64_t res= (int64_t)src * (int64_t) dest;
-	// printf("test %llx ",alu_mul((uint32_t) src, (uint32_t) dest, data_size));
-	printf("data_size %d ",data_size);
-	printf(" src 0x%x ",src);
-	printf(" dest 0x%x ",dest);
-	printf(" res 0x%llx  ",res & (0xFFFFFFFF >> (64 - data_size*2)));
-	return res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size*2));
+	// int64_t res= (int64_t)src * (int64_t) dest;
+	// // printf("test %llx ",alu_mul((uint32_t) src, (uint32_t) dest, data_size));
+	// printf("data_size %d ",data_size);
+	// printf(" src 0x%x ",src);
+	// printf(" dest 0x%x ",dest);
+	// printf(" res 0x%llx  ",res & (0xFFFFFFFF >> (64 - data_size*2)));
+	// return res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size*2));
+
+	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	assert(0);
+	return 0;
 #endif
 }
 
