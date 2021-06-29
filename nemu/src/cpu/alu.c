b81eeefb829871bd825c4dc775594640ad2406ff
monitor <tracer@njuics.org>
2021-06-29 16:47:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -337,6 +337,9 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_shl(src, dest, data_size);
 #else
+    printf("src 0x%x ",src);
+	printf(" dest 0x%x ",dest);
+	printf("src 0x%x ");
 	uint32_t res=0;
 	res=src<<dest;
 	set_CF_shl(src,data_size);
