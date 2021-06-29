273df5436f28823854c14be16363c5fcbf65d286
tracer-pa-public <tracer@njuics.org>
2021-06-29 15:35:31 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  15:35:31 up  5:12,  1 user,  load average: 0.01, 0.05, 0.01 d5f93ccae3c2d4ba11457f1ba5bfed20ae1ebb06

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -175,7 +175,6 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	else
 	{
 		set_CF_sub(src, dest, data_size);
-		uint32_t backup_res = res;
 		if (cpu.eflags.CF == 1)
 			res = res + 1;
 		else
@@ -185,7 +184,7 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
 	set_OF_sub(res, src, dest, data_size);
-	
+
 	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
