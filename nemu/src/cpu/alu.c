a7f7de7ba5d040983bc3f3d9699e25e33f774245
tracer-pa-public <tracer@njuics.org>
2021-06-29 21:41:56 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  21:41:56 up 1 day,  8:39,  2 users,  load average: 0.23, 0.16, 0.06 4040d8fcc1dc10b8d9b17d2c75fdb7459019468e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -263,12 +263,16 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_mul(src, dest, data_size);
 #else
 	uint64_t res = 0;
+	uint64_t test = 0xFFFFFFFFFFFFFFFF;
+
 	res = src * dest;
 	set_CF_OF_mul(res, data_size);
 	printf("data_size %d ", data_size);
 	printf(" src 0x%x ", src);
 	printf(" dest 0x%x ", dest);
 	printf(" res before 0x%llx  ", res);
+	printf(" test 0x%llx  ", test);
+
 	printf(" res 0x%llx  ", res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size)));
 	printf(" cpu.eflags.CF %x\n ", cpu.eflags.CF);
 	return res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size));
