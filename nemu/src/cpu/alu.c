8a883aa9103360a90e0c538861ab0ca0da9b0e67
monitor <tracer@njuics.org>
2021-06-29 12:15:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -27,9 +27,10 @@ void set_PF(uint32_t result)
 	return !one_bits % 2;
 }
 
-void set_SF(uint32_t res, size_t data_size){
+void set_SF(uint32_t result, size_t data_size){
 	result = sign_ext(result & (0xFFFFFFF >> (32 - data_size)), data_size);
-
+	return re
+	
 }
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
