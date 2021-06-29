32042020369c6ec0f678eff27a41f8fb7851297e
monitor <tracer@njuics.org>
2021-06-29 16:10:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -174,10 +174,6 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 		set_CF_sub(src, dest, data_size);
 	else
 	{
-		printf("CF %d",cpu.eflags.CF);
-		printf(" dest 0x%x",dest);
-		printf(" src 0x%x\n",src);
-
 		set_CF_sub(src, dest, data_size);
 		if (cpu.eflags.CF == 0)			
 			set_CF_sub(1, res, data_size);
