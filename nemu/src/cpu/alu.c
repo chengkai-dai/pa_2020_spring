791571b3e2600a7c471797709184e1796042d50b
monitor <tracer@njuics.org>
2021-06-29 12:25:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -31,30 +31,36 @@ void set_SF(uint32_t result, size_t data_size)
 {
 	assert(data_size != 0);
 	result = sign_ext(result & (0xFFFFFFF >> (32 - data_size)), data_size);
-	return (result & (1 << (data_size - 1)))>> (data_size-1);
+	return (result & (1 << (data_size - 1))) >> (data_size - 1);
 }
 
-void set_OF_add(uint32_t result, uint32_t src, uint32_t dest, size_t data_size){
+void set_OF_add(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
+{
 	switch (data_size)
 	{
 	case 8:
 		result = sign_ext(result & 0xFF, 8);
-		src=sign_ext(src & 0xFF, 8);
-		dest=sign_ext(dest & 0xFF, 8);
+		src = sign_ext(src & 0xFF, 8);
+		dest = sign_ext(dest & 0xFF, 8);
 		break;
 	case 16:
 		result = sign_ext(result & 0xFFFF, 16);
-		src=sign_ext(src & 0xFFFF, 16);
-		dest=sign_ext(dest & 0xFFFF, 16);
-		break;	
+		src = sign_ext(src & 0xFFFF, 16);
+		dest = sign_ext(dest & 0xFFFF, 16);
+		break;
 	default:
 		break;
 	}
 
-	if(sign(src)==sign(dest)){
-		if(sign(src)!=sign(result))
-		cpu.
+	if (sign(src) == sign(dest))
+	{
+		if (sign(src) != sign(result))
+			cpu.eflags.OF = 1;
+		else
+			cpu.eflags.OF = 0;
 	}
+	else
+		cpu.eflags.OF = 0;
 }
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
