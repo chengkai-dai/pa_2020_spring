e48ddd9d1ca941e91d926ba797d5c52013bb59b0
tracer-pa-public <tracer@njuics.org>
2021-06-29 17:05:45 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  17:05:45 up  6:42,  2 users,  load average: 0.07, 0.09, 0.12 aa0c47b2d9e4b37294e41c5dc0befdae241c2bd4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -17,7 +17,7 @@ void set_CF_sub(uint32_t src, uint32_t dest, size_t data_size)
 void set_CF_shl(uint32_t src, uint32_t dest, size_t data_size)
 {
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
-	printf("sign test 0x%x\n", src);
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	cpu.eflags.CF = sign(dest<<(src-1));
 }
 
