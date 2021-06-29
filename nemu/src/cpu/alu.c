f211b13b1763c564aca03837ffc8afa4eee1cac2
tracer-pa-public <tracer@njuics.org>
2021-06-29 22:07:21 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:07:21 up 1 day,  9:04,  2 users,  load average: 0.27, 0.18, 0.09 77409bd929fa577be70db058a2d7b538aaca699c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -317,14 +317,14 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 
 	// res = src * dest;
 	set_CF_OF_mul(res, data_size);
-	// printf("data_size %d ", data_size);
-	// printf(" src 0x%x ", src);
-	// printf(" dest 0x%x ", dest);
-	// printf(" res before 0x%llx  ", res);
+	printf("data_size %d ", data_size);
+	printf(" src 0x%x ", src);
+	printf(" dest 0x%x ", dest);
+	printf(" res before 0x%llx  ", res);
 
-	// printf(" res 0x%llx  ", res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size*2)));
-	// printf(" cpu.eflags.CF %x ", cpu.eflags.CF);
-	// printf(" cpu.eflags.OF %x\n ", cpu.eflags.OF);
+	printf(" res 0x%llx  ", res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size*2)));
+	printf(" cpu.eflags.CF %x ", cpu.eflags.CF);
+	printf(" cpu.eflags.OF %x\n ", cpu.eflags.OF);
 
 	return res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size*2));
 #endif
