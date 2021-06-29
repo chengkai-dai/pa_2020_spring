11fedd895042410d9b4daa51c3c3ffb842073539
monitor <tracer@njuics.org>
2021-06-29 12:02:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -13,15 +13,16 @@ void set_ZF(uint32_t result, size_t data_size)
 	cpu.eflags.ZF = (result == 0);
 }
 
-void set_PF(uint32_t result){
+void set_PF(uint32_t result)
+{
 
 	result = result & 0xFF;
-	size_t one_bits= 0;
-	while(result!=0){
-		one_bits+=result & 0x1;
-		
+	size_t one_bits = 0;
+	while (result != 0)
+	{
+		one_bits += result & 0x1;
+		result = result >> 1;
 	}
-
 }
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
