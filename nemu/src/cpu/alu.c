d9fab6cd454fc7d7d1ae6a209cf144e02457784e
tracer-pa-public <tracer@njuics.org>
2021-06-29 17:54:25 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  17:54:25 up  7:31,  3 users,  load average: 0.14, 0.08, 0.08 3a4f0e848572e507e0700c984a86357479ae2d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -409,6 +409,9 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sar(src, dest, data_size);
 #else
+	printf("data_size %d ", data_size);
+	printf(" src 0x%x ", src);
+	printf(" dest 0x%x ", dest);
 	uint32_t res = 0;
 	src = src & (0xFFFFFFFF >> (32 - data_size));
 	dest = dest & (0xFFFFFFFF >> (32 - data_size));
