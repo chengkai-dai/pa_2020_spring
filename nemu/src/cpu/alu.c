44e0d78ca0991b6fb3a0142bbfc283775c1bddcf
tracer-pa-public <tracer@njuics.org>
2021-06-29 17:30:09 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  17:30:09 up  7:06,  2 users,  load average: 0.21, 0.08, 0.07 4cff72127aa78e6ec0f7c94fdffd01d319ae6e4e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -387,7 +387,7 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = 0;
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	dest= sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
-		printf(" res 0x%x ",res);
+	printf(" dest 0x%x ",dest);
 
 	res = dest >> src;
 	printf(" res 0x%x ",res);
