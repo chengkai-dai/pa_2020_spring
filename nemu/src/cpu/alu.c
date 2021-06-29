cd01612af51747b832f8723bdc77ad86834ee33a
monitor <tracer@njuics.org>
2021-06-29 17:32:02 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -386,7 +386,8 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	uint32_t res = 0;
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
-	dest= sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	printf(" mask 0x%x ",(0xFFFFFFFF >> (32 - data_size)));
 	printf(" mask 0x%x ",(0xFFFFFFFF >> (32 - data_size)));
 
 	res = dest >> src;
