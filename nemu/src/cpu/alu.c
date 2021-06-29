979565ab5aad7a7e2a0728a8b5131c21c005723b
monitor <tracer@njuics.org>
2021-06-29 15:24:02 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -168,9 +168,24 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sbb(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = dest - src;
+	if (cpu.eflags.CF == 0)
+		set_CF_sub(src, dest, data_size);
+	else
+	{
+		set_CF_add(res, src, data_size);
+		uint32_t backup_res = res;
+		res = res + 1;
+		if (cpu.eflags.CF == 0)
+			set_CF_add(res, backup_res, data_size);
+	}
+	set_PF(res);
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	set_OF_add(res, src, dest, data_size);
+	return res & (0xFFFFFFFF >> (32 - data_size));
+	
 #endif
 }
 
