907c6a87ead702733a6af33c7fa49f425b72d5fa
monitor <tracer@njuics.org>
2021-07-09 14:20:05 +0800

    git monitor commit.

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -1,52 +1,56 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op()
+static void instr_execute_2op() 
 {
-        operand_read(&opr_src);
-        opr_dest.val = opr_src.val;
-        operand_write(&opr_dest);
+	operand_read(&opr_src);
+	opr_dest.val = opr_src.val;
+	operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(mov, r, rm, b)
-    make_instr_impl_2op(mov, r, rm, v)
-        make_instr_impl_2op(mov, rm, r, b)
-            make_instr_impl_2op(mov, rm, r, v)
-                make_instr_impl_2op(mov, i, rm, b)
-                    make_instr_impl_2op(mov, i, rm, v)
-                        make_instr_impl_2op(mov, i, r, b)
-                            make_instr_impl_2op(mov, i, r, v)
-                                make_instr_impl_2op(mov, a, o, b)
-                                    make_instr_impl_2op(mov, a, o, v)
-                                        make_instr_impl_2op(mov, o, a, b)
-                                            make_instr_impl_2op(mov, o, a, v)
+make_instr_impl_2op(mov, r, rm, v)
+make_instr_impl_2op(mov, rm, r, b)
+make_instr_impl_2op(mov, rm, r, v)
+make_instr_impl_2op(mov, i, rm, b)
+make_instr_impl_2op(mov, i, rm, v)
+make_instr_impl_2op(mov, i, r, b)
+make_instr_impl_2op(mov, i, r, v)
+make_instr_impl_2op(mov, a, o, b)
+make_instr_impl_2op(mov, a, o, v)
+make_instr_impl_2op(mov, o, a, b)
+make_instr_impl_2op(mov, o, a, v)
 
-                                                make_instr_func(mov_i2r_b)
-{
-        OPERAND imm, r;
+make_instr_func(mov_i2r_b){
+        OPERAND imm,r;
 
-        imm.type = OPR_IMM;
+        imm.type=OPR_IMM;
 
-        imm.addr = eip + 1;
+        imm.addr=eip+1;
+
+        imm.data_size=8;
+        r.data_size=8;
+        r.type=OPR_REG;
+        r.addr=
+        
+
 }
 
-make_instr_func(mov_zrm82r_v)
-{
-        int len = 1;
-        OPERAND r, rm;
-        r.data_size = data_size;
-        rm.data_size = 8;
-        len += modrm_r_rm(eip + 1, &r, &rm);
+make_instr_func(mov_zrm82r_v) {
+	int len = 1;
+	OPERAND r, rm;
+	r.data_size = data_size;
+	rm.data_size = 8;
+	len += modrm_r_rm(eip + 1, &r, &rm);
+	
+	operand_read(&rm);
+	r.val = rm.val;
+	operand_write(&r);
 
-        operand_read(&rm);
-        r.val = rm.val;
-        operand_write(&r);
-
-        print_asm_2("mov", "", len, &rm, &r);
-        return len;
+	print_asm_2("mov", "", len, &rm, &r);
+	return len;
 }
 
-make_instr_func(mov_zrm162r_l)
-{
+make_instr_func(mov_zrm162r_l) {
         int len = 1;
         OPERAND r, rm;
         r.data_size = 32;
@@ -56,27 +60,25 @@ make_instr_func(mov_zrm162r_l)
         operand_read(&rm);
         r.val = rm.val;
         operand_write(&r);
-        print_asm_2("mov", "", len, &rm, &r);
+	print_asm_2("mov", "", len, &rm, &r);
         return len;
 }
 
-make_instr_func(mov_srm82r_v)
-{
+make_instr_func(mov_srm82r_v) {
         int len = 1;
         OPERAND r, rm;
         r.data_size = data_size;
         rm.data_size = 8;
         len += modrm_r_rm(eip + 1, &r, &rm);
-
-        operand_read(&rm);
+        
+	operand_read(&rm);
         r.val = sign_ext(rm.val, 8);
         operand_write(&r);
-        print_asm_2("mov", "", len, &rm, &r);
+	print_asm_2("mov", "", len, &rm, &r);
         return len;
 }
 
-make_instr_func(mov_srm162r_l)
-{
+make_instr_func(mov_srm162r_l) {
         int len = 1;
         OPERAND r, rm;
         r.data_size = 32;
@@ -86,6 +88,6 @@ make_instr_func(mov_srm162r_l)
         r.val = sign_ext(rm.val, 16);
         operand_write(&r);
 
-        print_asm_2("mov", "", len, &rm, &r);
+	print_asm_2("mov", "", len, &rm, &r);
         return len;
 }
