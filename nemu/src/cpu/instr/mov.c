e6e7023abf7c6eb9efc74bb9bf8672f8c74281eb
monitor <tracer@njuics.org>
2021-07-09 14:21:04 +0800

    git monitor commit.

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -24,13 +24,16 @@ make_instr_func(mov_i2r_b){
         OPERAND imm,r;
 
         imm.type=OPR_IMM;
-
         imm.addr=eip+1;
-
         imm.data_size=8;
+        
         r.data_size=8;
         r.type=OPR_REG;
-        r.addr=
+        r.addr=opcode & 0x7;
+        operand_read(&imm);
+        r.val=imm.val;
+        operand_write(&r);
+        return 2;
         
 
 }
