c02d7d4816f08bdcaa0aaee0c9a5b8af8c5d5b6f
monitor <tracer@njuics.org>
2021-07-10 19:40:08 +0800

    git monitor commit. engkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  19:40:08 up 5 days,  2:15,  2 users,  load average: 0.45, 0.43, 0.38 a705f9d7473dd1b473536fac93fa48458bce42

--- nemu/src/cpu/instr/group.c
+++ nemu/src/cpu/instr/group.c
@@ -5,7 +5,7 @@
 	{                                                       \
 		uint8_t op_code;                                    \
 		modrm_opcode(eip + 1, &op_code);                    \
-		printf("opcode 0x%x\n",opcode);										\
+		printf("opcode 0x%x\n",op_code);										\
 		return concat(name, _entry)[op_code](eip, op_code); \
 	}
 #define make_group_impl_ref(name)                                    \
