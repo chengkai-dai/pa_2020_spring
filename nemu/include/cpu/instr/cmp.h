8a0299852383168e803e90eb2c42ce46326d8ae5
monitor <tracer@njuics.org>
2021-07-10 17:52:07 +0800

    git monitor commit.

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -1,9 +1,14 @@
 #ifndef __INSTR_CMP_H__
 #define __INSTR_CMP_H__
 
-
-
 make_instr_impl_2op(cmp, i, rm, v)
+
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	opr_dest.val = opr_src.val;
+	operand_write(&opr_dest);
+}
 
 
 #endif
