e5e28f17ac562f8f8c97c8db1ad19d92061ad662
monitor <tracer@njuics.org>
2021-07-10 17:25:07 +0800

    git monitor commit.

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -1,9 +1,9 @@
 #ifndef __INSTR_CMP_H__
 #define __INSTR_CMP_H__
 
-make_instr_func(jmp_near);
-make_instr_func(jnp_near);
-make_instr_func(jnb_near);
+make_instr_func();
+make_instr_func();
+make_instr_func();
 
 
 #endif
