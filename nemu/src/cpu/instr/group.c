79bebf1826ef614a64a9d653e505d06a9fefdad6
monitor <tracer@njuics.org>
2021-07-10 19:33:08 +0800

    git monitor commit.

--- nemu/src/cpu/instr/group.c
+++ nemu/src/cpu/instr/group.c
@@ -5,7 +5,6 @@
 	{                                                       \
 		uint8_t op_code;                                    \
 		modrm_opcode(eip + 1, &op_code);                    \
-		printf("opcode %d",opcode);										\
 		return concat(name, _entry)[op_code](eip, op_code); \
 	}
 #define make_group_impl_ref(name)                                    \
