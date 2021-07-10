dd15c2881eecd6c2bf353fb3b41d8c8fdf877e33
tracer-pa-public <tracer@njuics.org>
2021-07-10 17:59:02 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  17:59:02 up 5 days, 34 min,  2 users,  load average: 0.29, 0.31, 0.24 34078992a176ff24beb04edefe5bcc6fa635025a

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -3,12 +3,13 @@
 
 make_instr_impl_2op(cmp, i, rm, v)
 
-static void instr_execute_2op() 
-{
-	operand_read(&opr_src);
-	opr_dest.val = opr_src.val;
-	operand_write(&opr_dest);
-}
+
+// static void instr_execute_2op() 
+// {
+// 	operand_read(&opr_src);
+// 	opr_dest.val = opr_src.val;
+// 	operand_write(&opr_dest);
+// }
 
 
 #endif
