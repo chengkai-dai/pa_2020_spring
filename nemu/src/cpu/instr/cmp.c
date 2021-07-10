4c7acc1b8a2ba32f2df02f7d9723f6951a6a11fe
tracer-pa-public <tracer@njuics.org>
2021-07-10 20:01:56 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  20:01:56 up 5 days,  2:37,  2 users,  load average: 0.65, 0.50, 0.39 d731ed27dede6d681b6dc8c5aa690e992a319d3

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -5,15 +5,10 @@ make_instr_func(cmp_i2rm_bv){
 
     int len=modrm_rm(eip+1, &r);
     r.data_size=data_size;
-    printf("read len %d\n",len);
 
     imm.type=OPR_IMM;
-    imm.addr=eip+2;
+    imm.addr=eip+len;
     imm.data_size=8;
-
-    operand_read(&imm);
-    r.val=imm.val;
-    operand_write(&r);
 
     return 2;
 }
