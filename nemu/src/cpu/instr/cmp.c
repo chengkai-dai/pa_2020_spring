a3259497ae394f0f167a661de7cf4b328e141a26
monitor <tracer@njuics.org>
2021-07-10 19:50:07 +0800

    git monitor commit.

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,6 +1,18 @@
 #include "cpu/instr.h"
 
 make_instr_func(cmp_i2rm_bv){
-    printf("gotcha!\n");
-     return 2;
+    OPERAND imm, reg;
+    //         imm.type=OPR_IMM;
+//         imm.addr=eip+1;
+//         imm.data_size=8;
+
+//         r.data_size=8;
+//         r.type=OPR_REG;
+//         r.addr=opcode & 0x7;
+
+//         operand_read(&imm);
+//         r.val=imm.val;
+//         operand_write(&r);
+
+//         return 2;
 }
