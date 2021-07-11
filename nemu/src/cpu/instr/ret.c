299bc23a7d6a7a67b201e7a0e0fb5b9b2ce24b45
tracer-pa-public <tracer@njuics.org>
2021-07-11 21:22:02 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  21:22:02 up 17:56,  2 users,  load average: 0.38, 0.48, 0.55 c213a44b9803d123937ef5f38fa9458adb37cf6b

---
+++ nemu/src/cpu/instr/ret.c
@@ -0,0 +1,40 @@
+#include "cpu/instr.h"
+
+make_instr_func(ret_near){
+
+    int len = 1;
+    OPERAND rsp;
+
+    rsp.type = OPR_MEM;
+    rsp.addr = cpu.esp;
+    rsp.data_size = data_size;
+
+    operand_read(&rsp);
+
+    uint32_t ret_eip=rsp.val;
+
+    cpu.esp+=32;
+
+    cpu.eip=ret_eip;
+
+    printf("ret eip 0x%x\n",ret_eip);
+    printf("ret len %d\n",(int)ret_eip-(int)eip);
+    return (int)ret_eip-(int)eip;
+
+    // len += data_size / 8;
+
+    // OPERAND rel;
+    // rel.type = OPR_IMM;
+    // rel.sreg = SREG_CS;
+    // rel.data_size = data_size;
+    // rel.addr = eip + 1;
+
+    // operand_read(&rel);
+
+    // int offset = sign_ext(rel.val, data_size);
+
+    // cpu.eip += offset;
+    // printf("len %d\n",len);
+    // printf("offset 0x%x\n",offset);
+    // return len;
+}
