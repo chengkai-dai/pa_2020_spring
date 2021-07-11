d6aefd48ac00add689cdc1b7f2b9a388857d9cb4
tracer-pa-public <tracer@njuics.org>
2021-07-11 22:13:03 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:13:03 up 18:47,  2 users,  load average: 0.57, 0.53, 0.54 ede6e447a88c41fd1a889f0e6c674daa697ada7b

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -2,12 +2,21 @@
 
 make_instr_func(add_i2r_v){
 
-    OPERAND  r;
+    OPERAND  opr_src,opr_dest;
     int len = 1;
 
-    len += modrm_rm(eip + 1, &r);
-    printf("r.val 0x%x\n",r.val);
-    printf("r.addr 0x%x\n",r.addr);
+    len += opr_src.data_size / 8; 
+	opr_src.type = OPR_IMM;       
+	opr_src.sreg = SREG_CS;       
+	opr_src.addr = eip + 1;       
+    opr_dest.type = OPR_REG;      
+	opr_dest.addr = opcode & 0x7;
+    operand_read(&opr_src);
+    
+
+
+    printf("opr_src.val 0x%x\n",opr_src.val);
+    printf("opr_dest.addr 0x%x\n",opr_dest.addr);
     // r.data_size=data_size;
 
     // imm.type=OPR_IMM;
