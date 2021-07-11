92e9dd3679ad6b44b16a4d27c5bf98c5492f80dc
tracer-pa-public <tracer@njuics.org>
2021-07-11 11:46:10 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  11:46:10 up 12:19,  3 users,  load average: 0.60, 0.59, 0.59 f28299d69bcde99d60dc2a05c5da251a7afbd737

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -1,13 +1,13 @@
 #include "cpu/instr.h"
 
-make_instr_func(xor_i2a_b){
-    int len = 1; 
-	OPERAND al, imm;
-    al.type = OPR_REG;
-	al.data_size = 8;
-	al.addr = REG_AL;
+// make_instr_func(xor_i2a_b){
+//     int len = 1; 
+// 	OPERAND al, imm;
+//     al.type = OPR_REG;
+// 	al.data_size = 8;
+// 	al.addr = REG_AL;
 
 
 
-	operand_read(&al);
-}
+// 	operand_read(&al);
+// }
