402b5642d8bd9c10048364ddf51cdd616c1b58f8
tracer-pa-public <tracer@njuics.org>
2021-07-11 22:20:47 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:20:47 up 18:55,  2 users,  load average: 2.03, 0.88, 0.66 b4e55ba9c72b56862105b7cd6a879c0a18eba349

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -1,32 +1,32 @@
 #include "cpu/instr.h"
 
-make_instr_func(add_i2r_v){
+make_instr_func(add_i2r_v)
+{
 
-    OPERAND  opr_src,opr_dest;
+    OPERAND opr_src, opr_dest;
+
     int len = 1;
-    opr_src.data_size=data_size;
-    opr_dest.data_size=data_size;
-
-    len += opr_src.data_size / 8; 
-	opr_src.type = OPR_IMM;       
-	opr_src.sreg = SREG_CS;       
-	opr_src.addr = eip + 1;       
-    opr_dest.type = OPR_REG;      
-	opr_dest.addr = opcode & 0x7;
+    opr_src.data_size = data_size;
+    opr_src.type = OPR_IMM;
+    opr_src.sreg = SREG_CS;
+    opr_src.addr = eip + 1;
     operand_read(&opr_src);
-    
 
+    opr_dest.data_size = data_size;
+    opr_dest.type = OPR_REG;
+    opr_dest.addr = opcode & 0x7;
+    operand_read(&opr_src);
+    printf("opcode 0x%x\n", opcode);
 
-    printf("opr_src.val 0x%x\n",opr_src.val);
-    printf("opr_dest.addr 0x%x\n",opr_dest.addr);
+    printf("opr_src.val 0x%x\n", opr_src.val);
+    printf("opr_dest.addr 0x%x\n", opr_dest.addr);
     // r.data_size=data_size;
 
     // imm.type=OPR_IMM;
     // imm.addr=eip+len;
     // imm.data_size=8;
 
-
     // operand_read(&imm);
-	// alu_sub(imm.val,r.val,data_size);
-    return len+1;
+    // alu_sub(imm.val,r.val,data_size);
+    return len + 1;
 }
