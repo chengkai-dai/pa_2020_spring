71c8054f7839c3763626e36cdceed029c7b38960
tracer-pa-public <tracer@njuics.org>
2021-07-11 15:01:10 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  15:01:10 up 15:32,  2 users,  load average: 0.56, 0.56, 0.55 edcd585dc897084697974ce6408e6382f1d883fb

---
+++ nemu/src/cpu/instr/push.c
@@ -0,0 +1,16 @@
+#include "cpu/instr.h"
+
+make_instr_func(push_r_v){
+    OPERAND r, sr;
+    r.type = OPR_REG; 
+	r.addr = opcode & 0x7;
+    operand_read(&r);
+
+    sr.type = OPR_REG; 
+	sr.addr = 4;
+    operand_read(&sr);
+
+    printf("esp addr 0x%x\n", sr.mem_addr);
+
+    return 1;
+}
