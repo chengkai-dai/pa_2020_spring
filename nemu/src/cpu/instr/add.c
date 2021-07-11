04d82a110223bfa334dc6c4bf124daf7b073e979
tracer-pa-public <tracer@njuics.org>
2021-07-11 23:24:25 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  23:24:25 up 19:58,  2 users,  load average: 0.81, 0.63, 0.62 bdafcc993fcda31b4d1b9a2f8c9d52b1b53bdd9

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -3,22 +3,21 @@
 make_instr_func(add_i2rm_v)
 {
 
-    // OPERAND imm, r;
-    OPERAND r;
+    OPERAND imm, r;
+    // OPERAND r;
 
     int len = 1;
 
     len += modrm_rm(eip + 1, &r); 
 	r.data_size=data_size;
-
     operand_read(&r);
 
-    printf("r.addr 0x%x\n",r.addr);
-    printf("r.val 0x%x\n",r.val);
-    printf("r.mem_addr.base 0x%x\n",r.mem_addr.base);
-    printf("r.mem_addr.scale 0x%x\n",r.mem_addr.scale);
-    printf("r.mem_addr.index 0x%x\n",r.mem_addr.index);
-    printf("r.mem_addr.disp 0x%x\n",r.mem_addr.disp);
+    // printf("r.addr 0x%x\n",r.addr);
+    // printf("r.val 0x%x\n",r.val);
+    // printf("r.mem_addr.base 0x%x\n",r.mem_addr.base);
+    // printf("r.mem_addr.scale 0x%x\n",r.mem_addr.scale);
+    // printf("r.mem_addr.index 0x%x\n",r.mem_addr.index);
+    // printf("r.mem_addr.disp 0x%x\n",r.mem_addr.disp);
 
 
     // r.data_size = data_size;
@@ -28,17 +27,17 @@ make_instr_func(add_i2rm_v)
 
     // len += 1;
 
-    // imm.data_size = data_size;
-    // imm.type = OPR_IMM;
-    // imm.sreg = SREG_CS;
-    // imm.addr = eip + 2;
-    // operand_read(&imm);
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.sreg = SREG_CS;
+    imm.addr = eip + len;
+    operand_read(&imm);
 
-    // len += data_size / 8;
+    len += data_size / 8;
 
-    // r.val = alu_add(imm.val, r.val, data_size);
+    r.val = alu_add(imm.val, r.val, data_size);
 
-    // operand_write(&r);
+    operand_write(&r);
 
     return len;
 }
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,18 +1,19 @@
 #include "cpu/instr.h"
 
-make_instr_func(cmp_i2rm_bv){
+make_instr_func(cmp_i2rm_bv)
+{
     OPERAND imm, r;
     int len = 1;
 
     len += modrm_rm(eip + 1, &r);
-    r.data_size=data_size;
+    r.data_size = data_size;
+    operand_read(&r);
 
-    imm.type=OPR_IMM;
-    imm.addr=eip+len;
-    imm.data_size=8;
-
-
+    imm.type = OPR_IMM;
+    imm.addr = eip + len;
+    imm.data_size = 8;
     operand_read(&imm);
-	alu_sub(imm.val,r.val,data_size);
-    return len+1;
+
+    alu_sub(imm.val, r.val, data_size);
+    return len + 1;
 }
