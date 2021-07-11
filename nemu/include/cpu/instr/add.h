c6b1fab9aac88e1c9f0b79d07ad3fad035d2130c
tracer-pa-public <tracer@njuics.org>
2021-07-11 23:13:11 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  23:13:11 up 19:47,  2 users,  load average: 0.55, 0.53, 0.54 abb33ce6749fddd6536b4c4f93f2d089e3d198b2

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_ADD_H__
 #define __INSTR_ADD_H__
 
-make_instr_func(add_i2r_v);
+make_instr_func(add_i2rm_v);
 
 
 #endif
--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -1,30 +1,41 @@
 #include "cpu/instr.h"
 
-make_instr_func(add_i2r_v)
+make_instr_func(add_i2rm_v)
 {
 
     OPERAND imm, r;
 
     int len = 1;
 
-    r.data_size = data_size;
-    r.type = OPR_REG;
-    r.addr = instr_fetch(eip + 1, 1) & 0x7;
-    operand_read(&r);
+    len += modrm_rm(eip + 1, &r); 
+	r.data_size=data_size;
 
-    len += 1;
+    printf("r.addr 0x%x\n",r.addr);
+    printf("r.val 0x%x\n",r.val);
+    printf("r.mem_addr.base 0x%x\n",r.mem_addr.base);
+    printf("r.mem_addr.scale 0x%x\n",r.mem_addr.scale);
+    printf("r.mem_addr.index 0x%x\n",r.mem_addr.index);
+    printf("r.mem_addr.disp 0x%x\n",r.mem_addr.disp);
 
-    imm.data_size = data_size;
-    imm.type = OPR_IMM;
-    imm.sreg = SREG_CS;
-    imm.addr = eip + 2;
-    operand_read(&imm);
 
-    len += data_size / 8;
+    // r.data_size = data_size;
+    // r.type = OPR_REG;
+    // r.addr = instr_fetch(eip + 1, 1) & 0x7;
+    // operand_read(&r);
 
-    r.val = alu_add(imm.val, r.val, data_size);
+    // len += 1;
 
-    operand_write(&r);
+    // imm.data_size = data_size;
+    // imm.type = OPR_IMM;
+    // imm.sreg = SREG_CS;
+    // imm.addr = eip + 2;
+    // operand_read(&imm);
+
+    // len += data_size / 8;
+
+    // r.val = alu_add(imm.val, r.val, data_size);
+
+    // operand_write(&r);
 
     return len;
 }
