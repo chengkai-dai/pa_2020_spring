b3d3ffdc7e839483541299117654445b3b931b51
monitor <tracer@njuics.org>
2021-07-12 18:34:02 +0800

    git monitor commit.

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -3,8 +3,9 @@
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
+    operand_read(&opr_src);
 	opr_dest.val = opr_src.val;
 	operand_write(&opr_dest);
 }
 
-make_instr_impl_2op(and,i,rm,)
+make_instr_impl_2op(and,i,rm,bv)
