3791613193074f165952e6cd51bb166d20f4e9b7
monitor <tracer@njuics.org>
2021-07-12 15:43:02 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -5,13 +5,15 @@ make_instr_func(lea)
     int len=1;
     OPERAND rm,r;
     len += modrm_r_rm(eip + 1, &r, &rm);
+    operand_read(&r);
+    operand_read(&rm);
     printf("eip 0x%x\n",cpu.eip);
     printf("rm.mem_addr.base 0x%x\n", r.mem_addr.base);
-    printf("r.mem_addr.index 0x%x\n", r.mem_addr.index);
-    printf("r.mem_addr.scale 0x%x\n", r.mem_addr.scale);
-    printf("r.mem_addr.disp 0x%x\n", r.mem_addr.disp);
-    printf("r.addr 0x%x\n", r.addr);
-    printf("rm.addr 0x%x\n", rm.addr);
+    printf("rm.mem_addr.index 0x%x\n", r.mem_addr.index);
+    printf("rm.mem_addr.scale 0x%x\n", r.mem_addr.scale);
+    printf("rm.mem_addr.disp 0x%x\n", r.mem_addr.disp);
+    printf("rm.addr 0x%x\n", r.addr);
+    printf("r.addr 0x%x\n", rm.addr);
 
     return len;
 }
