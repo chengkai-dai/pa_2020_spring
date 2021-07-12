947291eaf92488c3e989e81b3dd09934b9bdffec
monitor <tracer@njuics.org>
2021-07-12 19:04:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -26,3 +26,26 @@ make_instr_func(push_r_v){
     return 1;
 }
 
+make_instr_func(push_rm_v){
+
+    cpu.esp-=data_size;
+
+    // step 2
+    OPERAND r, sr;
+    r.type = OPR_REG; 
+	r.addr = opcode & 0x7;
+    r.data_size=data_size;
+    operand_read(&r);
+
+    sr.type = OPR_MEM; 
+	sr.addr = cpu.esp;
+    sr.data_size = data_size;
+    sr.val=r.val;
+
+    operand_write(&sr);
+
+
+
+
+}
+
