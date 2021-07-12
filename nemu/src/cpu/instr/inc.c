e5709deb76936d3c625ade37ba25fdd6e5bf7b08
tracer-pa-public <tracer@njuics.org>
2021-07-12 16:47:44 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  16:47:44 up  5:00,  2 users,  load average: 0.54, 0.58, 0.52 15ffb01b1b9776c1a0dae5d82c3bd8bc2de3da16

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,7 +1,8 @@
 #include "cpu/instr.h"
 
 static void	instr_execute_1op(){
+    printf("come on baby!\n");
 
 }                                                                                                    \
 
-make_instr_impl_1op()
+make_instr_impl_1op(inc,rm,v);
