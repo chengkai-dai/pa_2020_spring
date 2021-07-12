bd63059c55deb26c7cfbd7c5954883af2b830665
monitor <tracer@njuics.org>
2021-07-12 17:57:02 +0800

    git monitor commit.

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -4,11 +4,11 @@ static void	instr_execute_1op(){
     opr_dest.val+=1;
     printf("opr_dest.addr 0x%x\n ", opr_dest.addr);
 
-    printf("opr_dest.addr 0x%x\n ", opr_dest.);
+    printf("opr_dest.val 0x%x\n ", opr_dest.val);
 
-    printf("opr_dest.addr 0x%x\n ", opr_dest.addr);
+    printf("opr_dest.mem_addr.base 0x%x\n ", opr_dest.mem_addr.base);
 
-    printf("opr_dest.addr 0x%x\n ", opr_dest.addr);
+    printf("opr_dest.mem_addr.index 0x%x\n ", opr_dest.mem_addr.index);
 
     printf("opr_dest.addr 0x%x\n ", opr_dest.addr);
 
