0b33f62fd6e28ef6cccb43da0ee3839283c3ae63
tracer-pa-public <tracer@njuics.org>
2021-07-12 11:16:56 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  11:16:56 up 6 days, 17:50,  3 users,  load average: 0.35, 0.36, 0.42 6ed26ec65dd482c53507ba3548dd6cbb4750832d

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -3,7 +3,10 @@
 static void instr_execute_1op_cc_pass()
 {
 	operand_read(&opr_src);
+	printf("before eip 0x%x\n",cpu.eip);
 	cpu.eip += sign_ext(opr_src.val, opr_src.data_size);
+	printf("after eip 0x%x\n",cpu.eip);
+
 }
 
 static void instr_execute_1op_cc_fail() {/* Do nothing */}
