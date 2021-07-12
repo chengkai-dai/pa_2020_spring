cbcb24c86e9f18137ae27908894a9c4906b3c889
tracer-pa-public <tracer@njuics.org>
2021-07-12 18:47:37 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  18:47:37 up  7:00,  2 users,  load average: 0.95, 0.90, 0.69 689209e4a5c36a77e2ac960c01148cabec2553b

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -4,12 +4,7 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
     operand_read(&opr_dest);
-    printf("opr_src.val 0x%x\n",sign_ext(opr_src.val,opr_src.data_size));
-    printf("opr_dest.val 0x%x\n",opr_dest.val);
-    printf("data size %d\n",data_size);
-
 	opr_dest.val=alu_and(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size);
-    printf("opr_dest.val 0x%x\n",opr_dest.val);
 
 	operand_write(&opr_dest);
 }
