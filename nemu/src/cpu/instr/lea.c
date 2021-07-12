fcfe185cc55feb0ec37a4a53aae1667386df5ed5
tracer-pa-public <tracer@njuics.org>
2021-07-12 16:25:17 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  16:25:17 up  4:38,  2 users,  load average: 0.38, 0.49, 0.47 9fa7bca9717c03cb28ecd47b39bdb3932c50fd

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -5,12 +5,9 @@ make_instr_func(lea)
     int len=1;
     OPERAND rm,r;
     len += modrm_r_rm(eip + 1, &r, &rm);
-    // operand_read(&r);
-    // operand_read(&rm);
     r.data_size=data_size;
     r.val = rm.addr;
     operand_write(&r);
-    printf("2222\n");
 
     return len;
 }
