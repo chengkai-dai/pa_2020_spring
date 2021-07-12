cb4fa3ea0f29241ae5708aa48abf30ac21e68b3b
tracer-pa-public <tracer@njuics.org>
2021-07-12 13:29:32 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  13:29:32 up  1:42,  3 users,  load average: 0.64, 0.70, 0.71 c4b5f318225966faf2d1877b3630fe7ad44e5ff6

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -11,7 +11,7 @@ static void instr_execute_1op_cc_pass()
 
 }
 
-static void instr_execute_1op_cc_fail() {printf("cpu.eip 0x%x\n",cpu.eip);}
+static void instr_execute_1op_cc_fail() {printf("fail\n");}
 
 #define jcc_helper(suffix) \
 	make_instr_impl_1op_cc(j, i, suffix, e) \
