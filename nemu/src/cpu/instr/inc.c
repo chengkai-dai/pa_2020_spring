5390f91d00301f57c90da700e38fef89c9108f41
monitor <tracer@njuics.org>
2021-07-12 16:47:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,3 +1,7 @@
 #include "cpu/instr.h"
 
+static void	instr_execute_1op(){
 
+}                                                                                                    \
+
+make_instr_impl_1op()
