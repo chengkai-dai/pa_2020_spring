ddcf7b9a26191be70d38c20618368642233ee751
monitor <tracer@njuics.org>
2021-07-12 16:49:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,8 +1,8 @@
 #include "cpu/instr.h"
 
 static void	instr_execute_1op(){
-    printf("come on baby!\n");
-
+    opr_dest.val+=1;
+    operand_write(&opr_dest);
 }                                                                                                    \
 
 make_instr_impl_1op(inc,rm,v);
