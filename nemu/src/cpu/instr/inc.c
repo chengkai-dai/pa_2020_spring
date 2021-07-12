9c62644add6fe4b01051522974c2965a01b43cdb
tracer-pa-public <tracer@njuics.org>
2021-07-12 18:17:36 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  18:17:36 up  6:30,  2 users,  load average: 0.57, 0.47, 0.47 9daaf9b769c10dbada86037fc1f757f8df118d2c

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,20 +1,20 @@
 #include "cpu/instr.h"
 
 static void	instr_execute_1op(){
-    opr_dest.val+=1;
-    printf("opr_dest.addr 0x%x\n ", opr_dest.addr);
+    opr_src.val+=1;
+    printf("opr_dest.addr 0x%x\n ", opr_src.addr);
 
-    printf("opr_dest.val 0x%x\n ", opr_dest.val);
+    printf("opr_dest.val 0x%x\n ", opr_src.val);
 
-    printf("opr_dest.mem_addr.base 0x%x\n ", opr_dest.mem_addr.base);
+    printf("opr_dest.mem_addr.base 0x%x\n ", opr_src.mem_addr.base);
 
-    printf("opr_dest.mem_addr.index 0x%x\n ", opr_dest.mem_addr.index);
+    printf("opr_dest.mem_addr.index 0x%x\n ", opr_src.mem_addr.index);
 
-    printf("opr_dest.scale 0x%x\n ", opr_dest.mem_addr.scale);
+    printf("opr_dest.scale 0x%x\n ", opr_src.mem_addr.scale);
 
-    printf("opr_dest.disp %d\n ", opr_dest.mem_addr.disp);
+    printf("opr_dest.disp %d\n ", opr_src.mem_addr.disp);
 
-    operand_write(&opr_dest);
+    operand_write(&opr_src);
 }
 
 make_instr_impl_1op(inc,rm,v);
