817e777a177b726488629f483306563cdc5d3023
monitor <tracer@njuics.org>
2021-07-12 15:05:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -28,6 +28,7 @@ extern uint8_t data_size;
 #include "cpu/instr/add.h"
 #include "cpu/instr/test.h"
 #include "cpu/instr/sub.h"
+#include "cpu/instr/lea.h"
 
 /* TODO: add more instructions here */
 
