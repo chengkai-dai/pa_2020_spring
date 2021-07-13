004d7154f118fcbdda921f1f7646c57552ed23ae
tracer-pa-public <tracer@njuics.org>
2021-07-13 22:01:44 +0800

    >  compile nemu testcase kernel 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:01:44 up  1:44,  3 users,  load average: 1.02, 0.64, 0.56 2c13bea7f2ff65d8ccc92a33332a60a4448715f9

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -4,8 +4,13 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+	printf("cf value 0x%x\n",cpu.eflags.CF);
+
+	printf("adc before value 0x%x\n",opr_dest.val);
 	opr_dest.val=alu_adc(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size);
-    operand_write(&opr_dest);
+	printf("adc after value 0x%x\n",opr_dest.val);
+
+	operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(adc, r, rm, v)
