140e8580197218279b44d04030d62470accbbc7e
tracer-pa-public <tracer@njuics.org>
2021-07-13 22:56:25 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:56:25 up 30 min,  2 users,  load average: 1.55, 0.83, 0.62 5e55fd37feb0446994e2aebc52f7235df680738

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -1,17 +1,10 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op() 
+static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	// printf("eax value 0x%x\n",cpu.gpr[0]._32);
-	// printf("ecx value 0x%x\n",cpu.gpr[1]._32);
-	// printf("cf value 0x%x\n",cpu.eflags.CF);
-
-	// printf("adc before value 0x%x\n",opr_dest.val);
-	opr_dest.val=alu_adc(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size);
-	// printf("adc after value 0x%x\n",opr_dest.val);
-
+	opr_dest.val = alu_adc(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
 	operand_write(&opr_dest);
 }
 
