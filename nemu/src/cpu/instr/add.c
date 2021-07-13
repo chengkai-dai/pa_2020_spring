d7fc661363237f9799d97dc1fab57aac90b71588
tracer-pa-public <tracer@njuics.org>
2021-07-13 22:53:16 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:53:16 up 27 min,  2 users,  load average: 0.65, 0.59, 0.53 eacee385521ca18f7067c0d4a24c8f6f47f81c

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -5,25 +5,26 @@ static void instr_execute_2op()
 	
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	if(cpu.eip==0x3003a){
-		printf("eax value before 0x%x\n",cpu.gpr[0]._32);
-		printf("ecx value before 0x%x\n",cpu.gpr[1]._32);
-		printf("add opr_src.val 0x%x\n", opr_src.val);
-		printf("add opr_dest.val before 0x%x\n", opr_dest.val);
-	}
+	// if(cpu.eip==0x3003a){
+	// 	printf("eax value before 0x%x\n",cpu.gpr[0]._32);
+	// 	printf("ecx value before 0x%x\n",cpu.gpr[1]._32);
+	// 	printf("add opr_src.val 0x%x\n", opr_src.val);
+	// 	printf("add opr_dest.val before 0x%x\n", opr_dest.val);
+	// }
     opr_dest.val = alu_add(sign_ext(opr_src.val,opr_src.data_size), opr_dest.val, data_size);
 	operand_write(&opr_dest);
-	if(cpu.eip==0x3003a){
-		printf("eax value after 0x%x\n",cpu.gpr[0]._32);
-		printf("ecx value after 0x%x\n",cpu.gpr[1]._32);
-		printf("add opr_dest.val after 0x%x\n", opr_dest.val);
-	}
+	// if(cpu.eip==0x3003a){
+	// 	printf("eax value after 0x%x\n",cpu.gpr[0]._32);
+	// 	printf("ecx value after 0x%x\n",cpu.gpr[1]._32);
+	// 	printf("add opr_dest.val after 0x%x\n", opr_dest.val);
+	// }
 }
 
 make_instr_impl_2op(add, i, rm, v)
 make_instr_impl_2op(add, i, a, v)
 make_instr_impl_2op(add, rm, r, v)
 make_instr_impl_2op(add, i, rm, bv)
+make_instr_impl_2op(add, r, rm, bv)
 
 
 // make_instr_func(add_i2rm_v)
