25f7291c7e85f85d8790110c950aa9ba17077645
tracer-pa-public <tracer@njuics.org>
2021-07-13 11:47:27 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  11:47:27 up 1 day,  7:41,  2 users,  load average: 0.66, 0.60, 0.59 f90522fac970c8776517754a95560c1b1fefe364

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,56 +1,56 @@
 #include "cpu/instr.h"
 
-make_instr_func(push_r_v){
+make_instr_func(push_r_v)
+{
 
-    int len=1;
+    int len = 1;
     // 1. allocate the space for the pushed value
     // 2. set the memory to the new value
 
     // step 1
 
-    cpu.esp-=data_size;
+    cpu.esp -= data_size / 8;
 
     // step 2
     OPERAND r, sr;
-    r.type = OPR_REG; 
-	r.addr = opcode & 0x7;
-    r.data_size=data_size;
+    r.type = OPR_REG;
+    r.addr = opcode & 0x7;
+    r.data_size = data_size;
     operand_read(&r);
 
-    sr.type = OPR_MEM; 
-	sr.addr = cpu.esp;
+    sr.type = OPR_MEM;
+    sr.addr = cpu.esp;
     sr.data_size = data_size;
-    sr.val=r.val;
+    sr.val = r.val;
 
     operand_write(&sr);
 
     return len;
 }
 
-make_instr_func(push_rm_v){
-    int len=1;
+make_instr_func(push_rm_v)
+{
+    int len = 1;
     // step 1
-    cpu.esp-=data_size;
+    cpu.esp -= data_size / 8;
 
     // step 2
-    OPERAND rm,sr;
-    len+=modrm_rm(eip + 1,&rm);
-    rm.data_size=data_size;
+    OPERAND rm, sr;
+    len += modrm_rm(eip + 1, &rm);
+    rm.data_size = data_size;
     operand_read(&rm);
 
-    // r.type = OPR_REG; 
-	// r.addr = opcode & 0x7;
+    // r.type = OPR_REG;
+    // r.addr = opcode & 0x7;
     // r.data_size=data_size;
     // operand_read(&r);
 
-    sr.type = OPR_MEM; 
-	sr.addr = cpu.esp;
+    sr.type = OPR_MEM;
+    sr.addr = cpu.esp;
     sr.data_size = data_size;
-    sr.val=rm.val;
+    sr.val = rm.val;
 
     operand_write(&sr);
 
     return len;
-
 }
-
