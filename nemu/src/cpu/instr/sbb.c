36ba446546931662cd08086c19f2c71e3481d4f1
tracer-pa-public <tracer@njuics.org>
2021-07-14 22:28:10 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:28:10 up  1:33,  2 users,  load average: 0.01, 0.02, 0.00 27b270dc7c20ba6f5b08917d2c31b3b52816d406

---
+++ nemu/src/cpu/instr/sbb.c
@@ -0,0 +1,20 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+    uint32_t v;
+	if (opr_dest.data_size != opr_src.data_size){
+		v=alu_add(sign_ext(opr_src.val, opr_src.data_size), cpu.eflags.CF, data_size);
+        opr_dest.val=alu_sub(v,sign_ext(opr_dest.val,opr_dest.data_size), data_size);
+    }
+	else{
+		v=alu_add(opr_src.val, cpu.eflags.CF, data_size);
+        opr_dest.val=alu_sub(v,opr_dest.val,data_size);
+    }
+
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(sar, rm, r, v)
