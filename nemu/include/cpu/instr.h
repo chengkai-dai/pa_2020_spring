89f079554d133e32cd78b64fc25998579fac3570
tracer-pa-public <tracer@njuics.org>
2021-07-14 21:09:10 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  21:09:10 up 14 min,  2 users,  load average: 0.11, 0.06, 0.02 4067d9032c52d2d5fc9712ed248e749ba2d01126

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -38,6 +38,7 @@ extern uint8_t data_size;
 #include "cpu/instr/leave.h"
 #include "cpu/instr/dec.h"
 #include "cpu/instr/set.h"
+#include "cpu/instr/not.h"
 
 /* TODO: add more instructions here */
 
---
+++ nemu/src/cpu/instr/not.c
@@ -0,0 +1,17 @@
+#include "cpu/instr.h"
+
+make_instr_func(not_rm_v)
+{
+    OPERAND rm;
+    int len = 1;
+
+    len += modrm_rm(eip + 1, &rm);
+    rm.data_size = data_size;
+    operand_read(&rm);
+
+    rm.val = ~rm.val;
+
+    operand_write(&rm);
+
+    return len + 1;
+}
