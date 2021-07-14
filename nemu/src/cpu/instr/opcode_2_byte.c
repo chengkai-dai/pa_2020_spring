74b4a140c22c2fae5425981ba872d7032eae3839
tracer-pa-public <tracer@njuics.org>
2021-07-14 15:08:51 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  15:08:51 up  5:26,  2 users,  load average: 0.26, 0.16, 0.07 8a470b2443b1708d274551b1659083a62aa37dbe

--- nemu/src/cpu/instr/opcode_2_byte.c
+++ nemu/src/cpu/instr/opcode_2_byte.c
@@ -7,8 +7,6 @@ make_instr_func(opcode_2_byte)
 #ifdef NEMU_REF_INSTR
 	len += __ref_opcode_2_byte_entry[op](eip + 1, op);
 #else
-	printf("come on!\n");
-	printf("op %d\n",op);
 	len += opcode_2_byte_entry[op](eip + 1, op);
 #endif
 	return len;
