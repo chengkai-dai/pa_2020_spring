5a471bc0c408be645816191a6e8b69456439c59b
tracer-pa-public <tracer@njuics.org>
2021-07-14 12:06:25 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  12:06:25 up  2:24,  2 users,  load average: 0.04, 0.05, 0.01 cbd2f556248795c230017087255b9d835df0063

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -56,9 +56,13 @@ void set_ZF(uint32_t result, size_t data_size)
 
 void set_PF(uint32_t result)
 {
+	if(cpu.eip==0x30018){
+		printf("result %0x%x\n",result);
+	}
 		
 	result = result & 0xFF;
 	size_t one_bits = 0;
+	
 	while (result != 0)
 	{
 		one_bits += result & 0x1;
