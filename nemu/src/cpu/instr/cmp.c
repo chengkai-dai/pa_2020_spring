b3c6c5b0672192b4d2f52f9a541358e5038a199e
tracer-pa-public <tracer@njuics.org>
2021-07-14 12:03:00 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  12:03:00 up  2:21,  2 users,  load average: 0.01, 0.04, 0.00 5907ba0c121edea9fa490a112be7a8b6e1060b74

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -4,8 +4,10 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	alu_sub(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
+	uint32_t val=alu_sub(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
+
 	if(cpu.eip==0x30018){
+		printf("val 0x%x\n",val);
 		printf("cpu.pf 0x%x\n",cpu.eflags.PF);
 	}
 	
