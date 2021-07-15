e5dc050ceeb93994108ee729fa142786c8aeb3d7
tracer-pa-public <tracer@njuics.org>
2021-07-15 09:22:33 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  09:22:33 up 28 min,  2 users,  load average: 0.42, 0.39, 0.29 1dedebd9759bc26063021d6d2383d4cf1002df09

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -4,18 +4,18 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
     operand_read(&opr_dest);
-	// opr_dest.val=alu_and(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size);
-	if (opr_dest.data_size != opr_src.data_size)
-	{
-		printf("opr_dest 0x%x\n",opr_dest.val);
-		printf("opr_src 0x%x\n",opr_src.val);
-		printf("res1 0x%x\n",alu_and(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), data_size));
-		printf("res2 0x%x\n",alu_and(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size));
-	}
-	if (opr_dest.data_size != opr_src.data_size)
-		alu_and(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), data_size);
-	else
-		alu_and(sign_ext(opr_src.val, data_size),opr_dest.val, data_size);
+	opr_dest.val=alu_and(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size);
+	// if (opr_dest.data_size != opr_src.data_size)
+	// {
+	// 	printf("opr_dest 0x%x\n",opr_dest.val);
+	// 	printf("opr_src 0x%x\n",opr_src.val);
+	// 	printf("res1 0x%x\n",alu_and(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), data_size));
+	// 	printf("res2 0x%x\n",alu_and(sign_ext(opr_src.val,opr_src.data_size),opr_dest.val,data_size));
+	// }
+	// if (opr_dest.data_size != opr_src.data_size)
+	// 	alu_and(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), data_size);
+	// else
+	// 	alu_and(sign_ext(opr_src.val, data_size),opr_dest.val, data_size);
 
 	operand_write(&opr_dest);
 }
