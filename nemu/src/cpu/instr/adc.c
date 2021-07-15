a26f6e3f4e1f91e4af252f67b528ca7438d3f88f
tracer-pa-public <tracer@njuics.org>
2021-07-15 09:15:33 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  09:15:33 up 21 min,  2 users,  load average: 1.21, 0.50, 0.28 99fe6fa517bf53bcbc753b12d0cf23659f587cd

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -5,12 +5,12 @@ static void instr_execute_2op()
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 
-	if (opr_dest.data_size != opr_src.data_size)
-		alu_adc(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), data_size);
-	else
-		alu_adc(opr_src.val, opr_dest.val, data_size);
-		
-	//opr_dest.val = alu_adc(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
+	// if (opr_dest.data_size != opr_src.data_size)
+	// 	alu_adc(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), data_size);
+	// else
+	// 	alu_adc(opr_src.val, opr_dest.val, data_size);
+
+	opr_dest.val = alu_adc(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
 	operand_write(&opr_dest);
 }
 
