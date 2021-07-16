c99ce683abf1a1fa5f2823ce5eac2ccb333fe27e
tracer-pa-public <tracer@njuics.org>
2021-07-16 21:53:42 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  21:53:42 up  1:20,  2 users,  load average: 0.00, 0.08, 0.05 9d8f6c94d6864347692f715cd098d172f1cbf7d

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -37,3 +37,40 @@ make_instr_func(call_near)
 
     return len;
 }
+
+#include "cpu/instr.h"
+
+make_instr_func(call_near_indirect)
+{
+    int len = 1;
+    OPERAND rsp;
+
+    // next instruction is pushed to the top of stack
+    // 1. allocate the space on the stack for next instruction
+    // 2. push the instruction to stack
+    // 3. jmp to the next instr
+
+    len += data_size / 8;
+
+    //step 1 and 2
+    cpu.esp -= data_size / 8;
+    rsp.type = OPR_MEM;
+    rsp.addr = cpu.esp;
+    rsp.data_size = data_size;
+    rsp.val = eip + len;
+
+    operand_write(&rsp);
+
+    //step 3
+    // set the eip to the call addr ((current eip + len) (next instr) + offset)
+
+    OPERAND ind;
+    ind.data_size = data_size;
+    len += modrm_rm(eip + 1, &ind);
+    operand_read(&ind);
+    int dest = sign_ext(ind.val, data_size);
+
+    cpu.eip += dest;
+
+    return 0;
+}
