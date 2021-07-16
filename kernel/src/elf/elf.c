c622463bd3670cc92e78a12c8a75d7318fd446e0
tracer-pa-public <tracer@njuics.org>
2021-07-16 21:33:06 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  21:33:06 up 59 min,  2 users,  load average: 0.00, 0.00, 0.00 49b7213edc13b98662b752f294bb25bd26a7ff6d

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -38,11 +38,15 @@ uint32_t loader()
 		{
 
 			// remove this panic!!!
-			panic("Please implement the loader");
+			// panic("Please implement the loader");
+			memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_memsz);
+			
+			if (ph->p_memsz > ph->p_filesz)
+				memset((void *)ph->p_vaddr + ph->p_filesz, 0, (ph->p_memsz - ph->p_filesz));
 
-/* TODO: copy the segment from the ELF file to its proper memory area */
+				/* TODO: copy the segment from the ELF file to its proper memory area */
 
-/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
+				/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
