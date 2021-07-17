cc2812f8aab67f4e493b5dc8644b7a322edc5de7
tracer-pa-public <tracer@njuics.org>
2021-07-17 22:14:36 +0800

    >  nemu 123456 daichengkai Linux debian 4.19.0-17-686-pae #1 SMP Debian 4.19.194-2 (2021-06-21) i686 GNU/Linux  22:14:36 up  8:55,  3 users,  load average: 0.22, 0.13, 0.04 6632b472723001cf8abfc90dc59805c4d46c483

--- nemu/src/monitor/ui.c
+++ nemu/src/monitor/ui.c
@@ -229,7 +229,7 @@ cmd_handler(cmd_x)
 	char * t;    
     for (t = args1; *t != '\0'; t++){
 		if (*t < '0' || *t > '9'){
-			puts("N should be a number!");
+			puts("N should be a decimal number!");
 			return 0;
 		}
 	}
