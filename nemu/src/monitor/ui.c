9b8321639fd4f335259b68542e16d4389c4f0944
monitor <tracer@njuics.org>
2021-07-17 14:10:06 +0800

    git monitor commit.

--- nemu/src/monitor/ui.c
+++ nemu/src/monitor/ui.c
@@ -221,14 +221,10 @@ cmd_handler(cmd_x)
 	}
 	printf("args %s\n",args);
 
-	char *steps = strtok(NULL, " ");
-	printf("steps %s\n",steps);
-
 	char *args2 = args + strlen(steps) + 1;
 	printf("args2 %s\n",args2);
 
-	char *args3 = strtok(NULL, " ");
-	printf("args3 %s\n",args3);
+	
 
 	// bool success;
 	// uint32_t val = expr(args, &success);
