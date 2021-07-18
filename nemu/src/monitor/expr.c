4c7dab7d8a5e54082f860f52dcb423b087e720e3
monitor <tracer@njuics.org>
2021-07-18 13:33:08 +0800

    git monitor commit.

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -230,7 +230,8 @@ static uint32_t eval(int s, int e, bool *success)
 			val1 = eval(p, op-1);
 			val2 = eval(op + 1, q);
 			switch(op_type) {
-				case '+': return val1 + val2;
+				case '+': 
+				return val1 + val2;
 				case ' '--': /* ...
 				case '*': /* ...
 				case '/': /* ... */
