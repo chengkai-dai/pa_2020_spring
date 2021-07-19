acec966cbc11cfa1c70706d04c4378e35ba09bf0
monitor <tracer@njuics.org>
2021-07-19 10:14:10 +0800

    git monitor commit.

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -306,6 +306,7 @@ uint32_t expr(char *e, bool *success)
 {
 	if (!(make_token(e) && valid_expr(0,nr_token-1)))
 	{
+		printf("\nPlease implement eval at eval\n");
 		*success = false;
 		return 0;
 	}
