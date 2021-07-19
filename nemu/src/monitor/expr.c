5540732a1e16ad42545040d7755395f28bcb3e0e
monitor <tracer@njuics.org>
2021-07-19 09:27:09 +0800

    git monitor commit.

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -167,7 +167,6 @@ static bool check_parentheses(int s, int e, bool *success)
 	stk[0] = tokens[s].type;
 	for (int i = s + 1; i <= e; i++)
 	{
-		if(tokens[i].type == NUM && tokens[i-1].type = '')
 		if (tokens[i].type != ')' && tokens[i].type != '(')
 			continue;
 
