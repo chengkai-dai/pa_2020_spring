a2c11ae6604adba4d94abd8869639d16dbf4290f
monitor <tracer@njuics.org>
2021-07-26 17:48:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -4,7 +4,7 @@ CacheLine cache[LINE_SIZE];
 
 void init_cache()
 {
-    cache=(CacheLine *)malloc()
+    cache = (CacheLine *)malloc(sizeof(CacheLine) * LINE_SIZE);
 }
 
 uint32_t cache_read(paddr_t paddr, size_t len)
@@ -13,7 +13,6 @@ uint32_t cache_read(paddr_t paddr, size_t len)
     return 0;
 }
 
-void cache_write(paddr_t paddr, size_t len, uint32_t data){
-
-     
+void cache_write(paddr_t paddr, size_t len, uint32_t data)
+{
 }
