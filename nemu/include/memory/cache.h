a17b384be8b0e2ba50036e21fded425eab94a30c
monitor <tracer@njuics.org>
2021-07-26 17:18:34 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -11,6 +11,8 @@ typedef struct
     char valid_bit;
 } CacheLine;
 
+void init_cache();
+
 uint32_t cache_read(paddr_t paddr, size_t len);
 
 void cache_write(paddr_t paddr, size_t len, uint32_t data);
