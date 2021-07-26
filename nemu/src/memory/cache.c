63b20f3dbd999ecc9b8453e028bd598cb5456153
monitor <tracer@njuics.org>
2021-07-26 18:52:02 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -13,22 +13,26 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 {
     CacheAddr caddr;
     caddr.paddr = paddr;
+    
     int sindex = (int)caddr.set_index;
     uint32_t boffset = caddr.offset;
     uint32_t tag = caddr.tag;
 
-    bool hit=false;
+    bool hit = false;
 
-    for(int i=0;i<SET_CAPACITY;++i){
-        if(tag==cache[SET_CAPACITY*sindex+i].tag){
-            hit=true;
+    for (int i = 0; i < SET_CAPACITY; ++i)
+    {
+        if (tag == cache[SET_CAPACITY * sindex + i].tag)
+        {
+            hit = true;
         }
         else
             continue;
     }
 
-    if(hit==false){
-        uint32_t data=paddr_read(paddr,len);
+    if (hit == false)
+    {
+        uint32_t data = paddr_read(paddr, len);
         return data;
     }
 }
