3c0de8f5316132d7af3385792b637068652b0fb1
monitor <tracer@njuics.org>
2021-07-26 18:39:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,7 +1,7 @@
 #include "memory/cache.h"
 #include <stdlib.h>
 
-CacheLine cache[1<<LINE_SIZE];
+CacheLine cache[1 << LINE_SIZE];
 
 void init_cache()
 {
@@ -13,9 +13,10 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 {
     CacheAddr caddr;
     caddr.paddr = paddr;
-    int sindex=(int) caddr.set_index;
-    uint32_t boffset=caddr.offset;
-    uint32_t tag=caddr.offset;
+    int sindex = (int)caddr.set_index;
+    uint32_t boffset = caddr.offset;
+    uint32_t tag = caddr.tag;
+    
 
     return 0;
 }
