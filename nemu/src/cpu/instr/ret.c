bc1e76cf7d43dbb91eac47e85d7efacd2dccb281
monitor <tracer@njuics.org>
2021-08-11 17:41:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -21,7 +21,7 @@ make_instr_func(ret_near)
     cpu.esp += data_size / 8;
     if (cpu.eip == 0x804904a)
     {
-        printf("ret esp 0x%x\n", cpu.esp);
+        printf("ret va 0x%x\n", cpu.esp);
     }
     //step 3
     uint32_t ret_eip = rsp.val;
