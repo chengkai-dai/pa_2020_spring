b0aafdfedba5b029321fccf1a6c9e761d1007872
monitor <tracer@njuics.org>
2021-08-11 10:01:01 +0800

    git monitor commit.

--- kernel/src/driver/ide/disk.c
+++ kernel/src/driver/ide/disk.c
@@ -69,6 +69,7 @@ void disk_do_read(void *buf, uint32_t sector)
 	int i;
 	for (i = 0; i < 512 / sizeof(uint32_t); i++)
 	{
+		printf("in_long 0x%x\n",in_long(IDE_PORT_BASE);
 		*(((uint32_t *)buf) + i) = in_long(IDE_PORT_BASE);
 	}
 #endif
