42e67a7a509275cb48bb60c9fc7093a9e661d120
monitor <tracer@njuics.org>
2021-08-11 20:55:02 +0800

    git monitor commit.

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -24,6 +24,7 @@ make_instr_func(out_b)
     printf("out_data 0x%x\n",out_data);
 
     pio_write(port_no, data_size / 8, out_data);
+    printf("out_data finished\n");
 
     print_asm_0("out al (\%dx)", "", len);
 
